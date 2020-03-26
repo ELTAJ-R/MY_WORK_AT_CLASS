17f5aa743541bdfe65633e6426f14a3146fa1cd4
Eltaj-R <eltacrecebov@mail.ru>
2020-03-26 13:59:46 +0300

    #12th Homework-Ongoing

---
+++ src/main/java/hw12/Human.java
@@ -0,0 +1,132 @@
+package hw12;
+
+import hw09.DayOfWeek;
+import hw09.Pet;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Human {
+    private String name;
+    private String surname;
+    private int year;
+    private int iq;
+    Pet pet;
+
+    HashMap<DayOfWeek, String> schedule = new HashMap<>();
+
+    public Human(String name, String surname, int year, int iq) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.iq = iq;
+    }
+
+    public Human() {
+
+    }
+
+    public Human(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public HashMap<DayOfWeek, String> getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(DayOfWeek day, String work) {
+        schedule.put(day, work);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getIq() {
+        return iq;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setIq(int iq) {
+        this.iq = iq;
+    }
+
+
+    public String show() {
+        return String.format("%s %s", getName(), getSurname());
+
+
+    }
+
+    public String greetPet() {
+        return String.format("Hello,%s", pet.getNickname());
+    }
+
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || object.getClass() != this.getClass()) {
+            return false;
+        }
+        Human test = (Human) object;
+        if (test.getName() != getName()) return false;
+        if (test.getSurname() != getSurname()) return false;
+        return true;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 13 * hash + Objects.hashCode(this.getName())
+                + Objects.hashCode(this.getSurname()) + Objects.hashCode(this.getIq());
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s,Surname: %s,Age: %d,IQ:%d,Schedule:%s "
+                , getName(), getSurname(), getYear(), getIq(), schedule);
+    }
+
+    @Override
+    protected void finalize() {
+        System.out.println("Human object deleted");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/hw12/MainDAO.java
@@ -0,0 +1,51 @@
+package hw12;
+
+import hw09.DomesticCat;
+
+public class MainDAO {
+
+    public static void main(String[] args) {
+        Human mother = new Human("Jane", "Karleone");
+
+        Human father = new Human("Vito", "Karleone");
+
+        Human anotherMother = new Human("Jane", "Mikelsons");
+
+        Human anotherFather = new Human("Mark", "Mikelsons");
+
+        Human child1 = new Human("Michael", "Karleone", 20, 90);
+
+        Human child2 = new Human("Carol", "Karleone", 15, 80);
+
+        Human child3 = new Human("Jane", "Karleone", 25, 60);
+        DomesticCat ex = new DomesticCat("Tom", 5, 75);
+        FamilyController test = new FamilyController();
+
+        Family first = new Family();
+        first.setFather(father);
+        first.setMother(mother);
+        test.saveFamily(first);
+        test.adoptChild(first, child1);
+        Family second = new Family();
+        second.setMother(anotherMother);
+        second.setFather(anotherFather);
+        second.addChild(child2);
+        test.saveFamily(second);
+        test.bornChild(second, "Tom", "Jane");
+        test.adoptChild(second, child3);
+        test.addPet(0, ex);
+        test.createNewFamily(mother, father);
+//        System.out.println(test.getPets(0));
+//        System.out.println(test.getFamilyByIndex(0));
+//        test.deleteChildrenOlderThan(18);
+//
+//        test.displayAllFamilies();
+//        test.getFamiliesBiggerThan(5);
+//        test.getFamiliesSmallerThan(6);
+        System.out.println(second.prettyFormat());
+
+
+    }
+
+
+}
