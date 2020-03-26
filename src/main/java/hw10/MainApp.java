b9b4d73e30d554dc1fd1108f8119a3fb267205d0
Eltaj-R <eltacrecebov@mail.ru>
2020-03-26 11:12:45 +0300

    #Update-26th March

--- src/main/java/hw10/MainApp.java
+++ src/main/java/hw10/MainApp.java
@@ -1,10 +1,8 @@
 package hw10;
 
-import java.text.ParseException;
-
 
 public class MainApp {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         Human child = new Human("Jack", "Karelone", "22/08/2015", 90);
         System.out.println(child.describeAge());
         System.out.println(child);
